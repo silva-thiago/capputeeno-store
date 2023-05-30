922508af62c95b8f88572cc395c4a1025d367bd9
T. J. Silva <thiago.silva@imd.ufrn.br>
2023-05-29 22:04:00 -0300

    refactor: update font system

--- capputeeno/src/app/layout.tsx
+++ capputeeno/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Saira } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const saira = Saira({ weight: ['400', '700'], subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={saira.className}>{children}</body>
     </html>
   )
 }
